fa6643c246ebf1e7e806beb657c041328a120116
Al Bur <albur2021@yahoo.com>
2021-09-15 00:16:33 -0400

    Add Form Comp and apply Validation Method inside

--- demo_1/src/App.js
+++ demo_1/src/App.js
@@ -1,16 +1,14 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 import React from "react";
-import FirstFun from "./components/FirstFun";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from "./components/Form";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-     {/* <FirstFun lucky={7} name="Nichole"/> */}
-     <Form/>
+      <Form />
     </div>
   );
 }
--- demo_1/src/components/Form.jsx
+++ demo_1/src/components/Form.jsx
@@ -1,53 +1,60 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-const Form = (props) => {
-  const [name, setName] = useState("");
-  const [type, setType] = useState("");
-  const [age, setAge] = useState("0");
-  const addPet = (event) => {
-    event.preventDefault();
-    // console.log(event.target[0].value);
-    // setName(event.target[0].value);
-    console.log(event);
-  };
-  return (
-    <div>
-      <h1>Tell me about your pet</h1>
-      <form onSubmit={addPet}>
+
+const Form = (props) =>{
+    const [title,setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [titleValid, setTitleValid] = useState("");
+    const [descValid, setDescValid] = useState("");
+    const myFormData = (e) =>{
+        e.preventDefault();
+        
+    };
+  
+    const handleTitle = (e) =>{
+        setTitle(e.target.value);
+          //validation funciton
+        if(e.target.value.length < 1){
+            setTitleValid("Title can not be Empty!");
+        }else if( e.target.value.length > 10){
+            setTitleValid("The title is no longer than 10 characters!")
+        }else{
+            setTitleValid("");
+        }
+    };
+    const handleDescription = (e) =>{
+        setDescription(e.target.value);
+          //validation funciton
+        if(e.target.value.length < 1){
+            setDescValid("Description can not be Empty!");
+        }else if( e.target.value.length < 10){
+            setDescValid("The description is NOT ENOUGH!");
+        }else{
+            setDescValid("");
+        }
+    };
+
+return(
+  <div>
+    <form onSubmit = { myFormData }>
         <div className="form-group">
-          <label>Name of Pet</label>
-          <input
-            type="text"
-            className="form-control"
-            onChange={(event) => setName(event.target.value)}
-          />
+            <label htmlFor="Title">Title</label>
+            <input type="text" name="Title" className="form-control" onChange={ handleTitle }/>
+            <p className="text-danger">{ titleValid }</p>
         </div>
         <div className="form-group">
-          <label>Type of Pet</label>
-          <input
-            type="text"
-            className="form-control"
-            onChange={(event) => setType(event.target.value)}
-          />
+            <label htmlFor="Description">Description</label>
+            <input type="text" name="Description" className="form-control" onChange={ handleDescription }/>
+            <p className="text-danger">{ descValid }</p>
         </div>
-        <div className="form-group">
-          <label>Age of Pet</label>
-          <input
-            type="number"
-            className="form-control"
-            onChange={(event) => setAge(event.target.value)}
-          />
-        </div>
-        <div>
-          <input type="submit" value="Submit" className="btn btn-primary" />
-        </div>
-      </form>
-      <h1>Your Pet Info:</h1>
-      <h3>Name: {name}</h3>
-      <h3>Type: {type}</h3>
-      <h3>Age: {age}</h3>
-    </div>
-  );
-};
+        <input type="submit" value="Submit" className="btn btn-primary mt-2"/>
+    </form>
+    <h1>Your Titles:</h1>
+    <h3>Title: { title }</h3>
+    <h3>Description: { description }</h3>
+  </div>
+  )
+}
+
 
 export default Form;

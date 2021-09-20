fd58b4f71db381fb30c1c7bbc3349ea3724a12a4
Al Bur <albur2021@yahoo.com>
2021-09-20 14:13:23 -0400

    Install axios to fetch api's data from the other server and and display the image, to install use this command in terminal: npm i axios

--- demo_1/src/components/ApiCall.jsx
+++ demo_1/src/components/ApiCall.jsx
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from 'react';
-
+import axios from 'axios';
 
 const ApiCall = (props) =>{
     const [fox, setFox] = useState({});
     //prevents from calling API many times (side effects), 
     //and runs only one time
-    useEffect(() =>{
-        fetch("https://randomfox.ca/floof/")
-        .then(response => response.json())
-        .then(response => setFox(response))
-        .then(err => console.log(err))
-    },[]);
-   
+    // useEffect(() =>{
+    //     fetch("https://randomfox.ca/floof/")
+    //     .then(response => response.json())
+    //     .then(response => setFox(response))
+    //     .then(err => console.log(err))
+    // },[]);
+    
+    //Axios is used to communicate with the backend 
+    //also supports the Promise API that is native to JS ES6
+    // It is a library which is used to make requests to an API, return data from the API,
+    //and then do things with that data in our React applicatio
+   useEffect(()=>{
+       axios.get("https://randomfox.ca/floof/")
+       .then(response => setFox(response.data));
+   },[]);
     return(
     <div>
         <h1>Test</h1>

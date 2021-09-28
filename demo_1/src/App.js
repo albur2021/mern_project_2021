b583136786ee4b35bdf177b7b103eb95df07fc81
Al Bur <albur2021@yahoo.com>
2021-09-28 16:57:59 -0400

    //Making a new Param to be called - make sure the path is unique and adecvate to its Comp

--- demo_1/src/App.js
+++ demo_1/src/App.js
@@ -6,9 +6,12 @@ import First from "./components/First";
 import Second from "./components/Second";
 import Third from "./components/Third";
 import { Router } from "@reach/router";
+import Param from "./components/Param";
+import NewParam from "./components/NewParam";
 
 //Import this dependency from node_module in terminal
 //by: npm i @reach/router --force
+//Making a new Param to be called - make sure the path is unique and adecvate to its Comp
 function App() {
   return (
     <div className="App">
@@ -16,6 +19,8 @@ function App() {
         <First  path="/"/>
         <Second path="/second"/>
         <Third  path="/third"/>
+        <Param path="/:num"/>
+        <NewParam path="/num/:num"/>
       </Router>
     </div>
   );

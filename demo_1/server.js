65f19e98b656834821c0d182c0c011e6066683d2
Al Bur <albur2021@yahoo.com>
2021-10-04 20:40:17 -0400

    Import 'app.use( express.json()) and app.use( express.urlencoded({ extended: true})); ' to display Data from Post on terminal to Test Back End

--- demo_1/server.js
+++ demo_1/server.js
@@ -3,6 +3,11 @@ const express = require("express");//import express
 const app = express();// app - connect all routes
 const port = 8000;// port route where you display UI
 
+//Get Data from Post Request
+// Important - should be above all codes to make it running !
+app.use( express.json());
+app.use( express.urlencoded({ extended: true}));
+
 //This backend is primarely for interacting with an API
 app.get("/", (req, res)=> {
     res.json({ message: "This is from page"});
@@ -11,7 +16,18 @@ app.get("/api", (req, res)=> {
     res.json({ message: "Hello from the express backend! This is change! This is applied with nodemon running!"});
 });
 
-// We need some code that  will start our server
+app.post("/request",(req,res)=>{
+    console.log(req.body);
+    res.json({status:"ok"});
+});
+// adding Post with parameters
+app.post("/request/:id", (req, res)=>{
+    console.log(req.body);
+    console.log(req.params.id);
+    res.json({status: req.params.id});
+});
+
+// We need some code, that  will start our server
 app.listen(port,()=> console.log(`Running on port ${port}!!`));
 
 //1. To see the message/response/ from this server, use this command

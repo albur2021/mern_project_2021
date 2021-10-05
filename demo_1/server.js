dabc273660a520c529f368ecbc6c6da343b4ee98
Al Bur <albur2021@yahoo.com>
2021-10-04 21:25:49 -0400

    Create Server.js, import 'express' and 'faker API' to generate Randome Data to test that Data through Post App by using Classes

--- demo_1/server.js
+++ demo_1/server.js
@@ -1,42 +1,42 @@
-//Building out my Express BackEnd
-const express = require("express");//import express
-const app = express();// app - connect all routes
-const port = 8000;// port route where you display UI
+//npm i faker - generating free Data
+//Faker API it's a collection of completely free APIs that helps web developers and web designers generate fake data in a fast and easy way.
+const express = require("express");//import
+const app = express();
+const port = 8000;
+const faker = require("faker");//import 
 
-//Get Data from Post Request
-// Important - should be above all codes to make it running !
 app.use( express.json());
-app.use( express.urlencoded({ extended: true}));
+app.use( express.json());
+//console.log(`My dog's name is: ${faker.name.firstName()}, and dog's spiece is: ${faker.animal.dog()}`);
 
-//This backend is primarely for interacting with an API
-app.get("/", (req, res)=> {
-    res.json({ message: "This is from page"});
+class Dog {
+  constructor() {
+    this.name = faker.name.firstName();
+    this.breed = faker.animal.dog();
+  }
+}
+class Crocodile {
+  constructor() {
+    this.name = faker.name.firstName();
+    this.breed = faker.animal.crocodilia();
+  }
+}
+app.get("/dog", (req, res) => {
+  const dog = new Dog();
+  const croco = new Crocodile();
+  res.json(dog);
+  res.json(croco);
 });
-app.get("/api", (req, res)=> {
-    res.json({ message: "Hello from the express backend! This is change! This is applied with nodemon running!"});
+app.get("/croco", (req, res) => {
+  const croco = new Crocodile();
+  res.json(croco);
 });
 
-app.post("/request",(req,res)=>{
-    console.log(req.body);
-    res.json({status:"ok"});
-});
-// adding Post with parameters
-app.post("/request/:id", (req, res)=>{
-    console.log(req.body);
-    console.log(req.params.id);
-    res.json({status: req.params.id});
-});
+// Displaying multiple Items/Classes
+app.get("/", (req, res) => {
+    const croco = new Crocodile();
+    const dog = new Dog();
+    res.json({dog: dog, croco: croco});
+})
 
-// We need some code, that  will start our server
-app.listen(port,()=> console.log(`Running on port ${port}!!`));
-
-//1. To see the message/response/ from this server, use this command
-//http://localhost:8000/api
-//2. or from terminal use this command:
-// node server.js
-
-//3. To UPDATE any changes on messages from the server you need to Install nodemone, use this command
-// npm install -g nodemon
-// check version: npm nodemon --version
-// to run nodemone to see "This is change!" added to message, use this command:
-// nodemon server.js
+app.listen(port, () => console.log(`Listening on port ${port}!`));
